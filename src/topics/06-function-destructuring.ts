e84cedafe81753d9830624cac1077b7163fcc6b7
Edson337 <edson.miguel733@gmail.com>
2025-09-03 14:46:21 -0600

    Importaciones y exportaciones

--- src/topics/06-function-destructuring.ts
+++ src/topics/06-function-destructuring.ts
@@ -1,17 +1,17 @@
-interface Product {
+export interface Product {
     description: string;
     price: number;
 }
 
-const phone: Product = {
-    description: 'Nokia A1',
-    price: 150.0
-}
+// const phone: Product = {
+//     description: 'Nokia A1',
+//     price: 150.0
+// }
 
-const tablet: Product = {
-    description: 'iPad Air',
-    price: 250.0
-}
+// const tablet: Product = {
+//     description: 'iPad Air',
+//     price: 250.0
+// }
 
 interface TaxCalculationOptions {
     tax: number;
@@ -20,7 +20,7 @@ interface TaxCalculationOptions {
 
 // function taxCalculation(options: TaxCalculationOptions): [number, number] {
 // function taxCalculation({tax, products}: TaxCalculationOptions): [number, number] {
-function taxCalculation(options: TaxCalculationOptions): [number, number] {
+export function taxCalculation(options: TaxCalculationOptions): [number, number] {
     const { tax, products } = options;
     let total = 0;
     products.forEach(({price}) => {
@@ -29,12 +29,10 @@ function taxCalculation(options: TaxCalculationOptions): [number, number] {
     return [total, total * tax];
 }
 
-const shoppingCart = [phone, tablet];
-const tax = 0.15;
+// const shoppingCart = [phone, tablet];
+// const tax = 0.15;
 
-const [total, taxTotal] = taxCalculation({ products: shoppingCart, tax});
+// const [total, taxTotal] = taxCalculation({ products: shoppingCart, tax});
 
-console.log('Total:', total);
-console.log('Tax:', taxTotal);
-
-export {};
+// console.log('Total:', total);
+// console.log('Tax:', taxTotal);
---
+++ src/topics/07-import-export.ts
@@ -0,0 +1,17 @@
+import {Product, taxCalculation} from './06-function-destructuring';
+
+const shoppingCart: Product[] = [
+    {
+        description: 'Nokia A1',
+        price: 150.0
+    },
+    {
+        description: 'iPad Air',
+        price: 250.0
+    }
+];
+
+const [total, tax] = taxCalculation({ products: shoppingCart, tax: 0.15 });
+
+console.log('Total:', total);
+console.log('Tax:', tax);
