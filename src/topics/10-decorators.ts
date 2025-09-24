5da90a04887d213a6e726eb7417553a097c6d43c
Edson337 <edson.miguel733@gmail.com>
2025-09-24 16:54:11 -0600

    Decoradores

---
+++ src/topics/10-decorators.ts
@@ -0,0 +1,20 @@
+function classDecorator<T extends {new (...args: any[]): any}>(constructor: T) {
+    return class extends constructor {
+        newProperty = 'New Property';
+        hello = 'Override';
+    }
+}
+
+// @classDecorator
+class SuperClass {
+    public myProperty: string = 'Abc123';
+    
+    print() {
+        console.log('Hello World!');
+    }
+}
+
+console.log(SuperClass);
+
+const myClass = new SuperClass();
+console.log(myClass);
